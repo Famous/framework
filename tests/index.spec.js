8e35cdaa5b8f20c52b4e278dbd5d6d1d474e51a3
Imti <imtiazmaj@gmail.com>
2015-02-27 12:59:27 -0800

    tests: add tests for operations

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -10,9 +10,12 @@ test('StateManager', function(t) {
 
   var dogState = {
     age: 4,
+    name: 'Yorkie',
     breed: 'Yorkshire Terrier',
     isSleeping: false,
-    isHungry: true
+    isHungry: true,
+    cutenessLevel: 8.1,
+    playfulnessLevel: 8.8
   }
 
   var globalObserverFlag = false;
@@ -50,6 +53,49 @@ test('StateManager', function(t) {
   t.equal(ageObserverFlag, true);
   t.equal(SM._observerExists('age', ageObserver), true);
   t.equal(SM._observerExists('breed', ageObserver), false);
+
+  console.log('OPERATION');
+  SM.add('age', 1);
+  t.equal(SM.get('age'), 6);
+  SM.subtract('age', 1);
+  t.equal(SM.get('age'), 5);
+  SM.multiply('age', 2);
+  t.equal(SM.get('age'), 10);
+  SM.timesPI('age');
+  t.equal(SM.get('age'), 10 * Math.PI);
+  SM.divide('age', 2 * Math.PI);
+  t.equal(SM.get('age'), 5);
+  SM.pow('age', 2);
+  t.equal(SM.get('age'), 25);
+  SM.sqrt('age');
+  t.equal(SM.get('age'), 5);
+  SM.multiply('age', -1);
+  SM.abs('age');
+  t.equal(SM.get('age'), 5);
+  SM.sin('age');
+  t.equal(SM.get('age'), Math.sin(5));
+  SM.cos('age');
+  t.equal(SM.get('age'), Math.cos(Math.sin(5)));
+  SM.tan('age');
+  t.equal(SM.get('age'), Math.tan(Math.cos(Math.sin(5))));
+  SM.ceil('playfulnessLevel');
+  t.equal(SM.get('playfulnessLevel'), 8);
+  SM.ceil('cutenessLevel');
+  t.equal(SM.get('cutenessLevel'), 8);
+  SM.concat('name', 'Yorkie');
+  t.equal(SM.get('name'), 'YorkieYorkie');
+  SM.substring('name', [0, 6]);
+  t.equal(SM.get('name'), 'Yorkie');
+  SM.toUpper('name');
+  t.equal(SM.get('name'), 'YORKIE');
+  SM.toLower('name');
+  t.equal(SM.get('name'), 'yorkie');
+  SM.flip('isHungry');
+  t.equal(SM.get('isHungry'), false);
+  SM.toInt('isHungry');
+  SM.addOperator('triple', function (a) { return 3 * a });
+  SM.operate('cutenessLevel', null, 'triple');
+  t.equal(SM.get('cutenessLevel'), 24);
 
   t.end();
 });
