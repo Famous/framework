b9d6187788f4407c80cdd944d8bc1fbda91d03ba
Jordan Papaleo <papaleowebdev@gmail.com>
2015-03-17 10:12:18 -0700

    added backface-visible property to be available in the behaviors

--- components/famous/html-element/html-element.js
+++ components/famous/html-element/html-element.js
@@ -51,6 +51,21 @@ BEST.component('famous:html-element', {
                     });
                 }
             },
+            'backface-visible': function(state, message) {
+                var style = {
+                    '-webkit-backface-visibility': 'visible',
+                    'backface-visibility': 'visible'
+                };
+
+                if(!message) {
+                    style = {
+                        '-webkit-backface-visibility': 'hidden',
+                        'backface-visibility': 'hidden'
+                    }
+                }
+
+                state.set('style', style);
+            },
             'true-size': function(state, message) {
                 state.set('trueSize', true);
             }
