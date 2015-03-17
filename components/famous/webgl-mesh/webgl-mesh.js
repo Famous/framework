27fcb0f9194c9889ec88eb4ae71ca38772814a46
Imti <imtiazmaj@gmail.com>
2015-03-16 17:41:39 -0700

    feat: add webgl-mesh behaviors

--- components/famous/webgl-mesh/webgl-mesh.js
+++ components/famous/webgl-mesh/webgl-mesh.js
@@ -1,1 +1,33 @@
-BEST.component('famous:webgl-mesh', {});
+BEST.component('famous:webgl-mesh', {
+    behaviors: {
+        '$self': {
+            '$self:size': function(size) {
+                return size;
+            },
+            '$self:color': function(color) {
+                return color;
+            },
+            '$self:origin': function(origin) {
+                return origin;
+            },
+            '$self:normals': function(normals) {
+                return normals;
+            },
+            '$self:position': function(position) {
+                return position;
+            },
+            '$self:geometry': function(geometry) {
+                return geometry;
+            },
+            '$self:glossiness': function(glossiness) {
+                return glossiness;
+            },
+            '$self:metallness': function(metallness) {
+                return metallness;
+            },
+            '$self:flatShading': function(flatShading) {
+                return flatShading;
+            }
+        }
+    }
+});
