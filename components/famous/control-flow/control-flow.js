ccdbf847f61a3f00ded6b403a35d36313c416a5e
Arkady Pevzner <arkady@famo.us>
2015-03-18 09:21:05 -0700

    Check for Array inside of control-flow yield; hack

--- components/famous/control-flow/control-flow.js
+++ components/famous/control-flow/control-flow.js
@@ -9,7 +9,21 @@ BEST.component('famous:control-flow', {
             },
             'yield': function($domNode, $payload) {
                 while ($domNode.firstChild) $domNode.removeChild($domNode.firstChild);
-                while ($payload.length > 0) $domNode.appendChild($payload[0]);
+
+                /*
+                    HACK: This condition should be removed to enforce API consistency.
+                    By the time that the $payload gets to this point, it should be a
+                    NodeList and the developer using yield should either pass back a
+                    NodeList or a CSS Selector.
+                 */
+                if (Array.isArray($payload)) {
+                    for(var i = 0; i < $payload.length; i++) {
+                        $domNode.appendChild($payload[i]);
+                    }
+                }
+                else {
+                    while ($payload.length > 0) $domNode.appendChild($payload[0]);
+                }
             }
         }
     }
