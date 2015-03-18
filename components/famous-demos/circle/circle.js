42bec2dd9dca78b7606bc5e8da6129d5a8a396d4
Arkady Pevzner <arkady@famo.us>
2015-03-18 14:55:06 -0700

    Update famous-demos:circle to use new yield

--- components/famous-demos/circle/circle.js
+++ components/famous-demos/circle/circle.js
@@ -30,18 +30,13 @@ BEST.component('famous-demos:circle', {
             'unselectable': true
         },
         '#label' : {
-            'yield' : function(_surrogates) {
-                return _surrogates;
-            }
+            '$yield' : true
         }
     },
     events: {
         public: {
             'handle-click': function(state, message) {
                 state.setState('radius', state.getState('radius') + 5);
-            },
-            'yield' : function(state, _surrogates) {
-                state.set('_surrogates', _surrogates);
             }
         }
     },
