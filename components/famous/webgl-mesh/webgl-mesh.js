ec79e07c782547665e3ecb4a05956660e3d97ef7
Imti <imtiazmaj@gmail.com>
2015-03-18 13:03:51 -0700

    chore: cleanup webgl-mesh

--- components/famous/webgl-mesh/webgl-mesh.js
+++ components/famous/webgl-mesh/webgl-mesh.js
@@ -51,43 +51,16 @@ BEST.component('famous:webgl-mesh', {
         handlers: {
             'color': function($webGLMesh, $payload) {
                 // setBaseColor takes in 4 arguments not in an array
-                //console.log('setting base color: ', $payload);
-                //console.log('webGLMesh: ', $webGLMesh)
                 if ($payload instanceof Array)
                     $webGLMesh.setBaseColor($payload[1], $payload[2], $payload[3]);
                 else
                     $webGLMesh.setBaseColor($payload);
-                //console.log('webGLMesh get geometry: ', $webGLMesh.getGeometry());
             },
             'normals': function($webGLMesh, $payload) {
                 $webGLMesh.setNormals($payload);
             },
             'geometry': function($webGLMesh, $payload) {
-                var testGeometry = {
-                    buffers: [
-                        { name: 'pos', data: [-1, 1, 0, 0, -1, 0, 1, 1, 0] },
-                        { name: 'texCoord', data: [0, 0, 0.5, 1, 1, 0], size: 2 },
-                        { name: 'normals', data: [0, 0, 0] },
-                        { name: 'indices', data: [0, 1, 2], size: 1}
-                    ]
-                }
-                var testSpec = {
-                    spec: {
-                        id: 1,
-                        dynamic: false,
-                        type: 'TRIANGLES',
-                        bufferNames: ['pos', 'texCoord', 'normals', 'indices'],
-                        bufferValues: [[-1, 1, 0, 0, -1, 0, 1, 1, 0], [0, 0, 0.5, 1, 1, 0], [0, 0, 0], [0, 1, 2]],
-                        bufferSpacings: [3, 2, 3, 1],
-                        invalidations: [1, 2, 3, 4]
-                    },
-                    options: testGeometry.buffers,
-                    id: 1,
-                    DEFAULT_BUFFER_SIZE: 3
-                }
-                //$webGLMesh.setGeometry($payload);
-                console.log('webGLMesh: ', $webGLMesh);
-                $webGLMesh.setGeometry('Box');
+                $webGLMesh.setGeometry($payload);
             },
             'glossiness': function($webGLMesh, $payload) {
                 $webGLMesh.setGlossiness($payload);
