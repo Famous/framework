f3011326e1d092843f04cde2b6b1fc3612a9e0ac
Imti <imtiazmaj@gmail.com>
2015-03-18 17:41:22 -0700

    chore: cleanup + add todo

--- components/famous/webgl-geometries/webgl-geometries.js
+++ components/famous/webgl-geometries/webgl-geometries.js
@@ -1,12 +1,7 @@
 BEST.component('famous:webgl-geometries', {
     events: {
         handlers: {
-            'box': function($box, $payload) {
-                console.log('box: ', $box)
-                console.log('payload: ', $payload)
-                debugger;
-                return $box;
-            }
+            // TODO: add webgl-geomtries' handlers
         }
     }
 });
