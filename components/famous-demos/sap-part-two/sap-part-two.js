9e0bd323ca51884ae22f50133672fad9f0763ec5
Imti <imtiazmaj@gmail.com>
2015-03-26 15:10:47 -0700

    demo: layout sap-part-two

--- components/famous-demos/sap-part-two/sap-part-two.js
+++ components/famous-demos/sap-part-two/sap-part-two.js
@@ -1,6 +1,75 @@
 BEST.component('famous-demos:sap-part-two', {
     tree: 'sap-part-two.html',
-    behaviors: {},
+    behaviors: {
+        '#part-two-container': {
+            'size': [300, 600]
+        },
+        '#image-a-container': {
+            'size': [300, 300],
+            'position': function(imageAPosition) {
+                return imageAPosition;
+            }
+        },
+        '#image-a': {
+            'true-size': true,
+            'style': {
+                'background-color': '#FF0099',
+                'color': 'white'
+            }
+        },
+        '#image-b-container': {
+            'position': function(imageBPosition) {
+                return imageBPosition;
+            }
+        },
+        '#image-b': {
+            'true-size': true,
+            'style': {
+            }
+
+        },
+        '#simple-container': {
+            'position': function (simplePosition) {
+                return simplePosition;
+            }
+        },
+        '#simple': {
+            'style': {
+                'color': 'white',
+                'font-size': '40px',
+                'text-align': 'center',
+                'font-weight': 'bold'
+            }
+        },
+        '#has-a-life-container': {
+            'position': function(hasALifePosition) {
+                return hasALifePosition;
+            }
+        },
+        '#has-a-life': {
+            'style': {
+                'color': 'white',
+                'font-size': '20px',
+                'text-align': 'center',
+                'font-weight': 'bold'
+            }
+        },
+        '#logo-container': {
+            'size': function(logoSize) {
+                return logoSize;
+            },
+            'position': function(logoPosition) {
+
+            }
+        }
+    },
     events: {},
-    states: {}
+    states: {
+        imageAPosition: [0, 0],
+        imageBPosition: [0, 340],
+        simplePosition: [0, 100],
+        hasALifePosition: [0, 150],
+        logoPosition: [103, 340],
+        logoSize: [108, 47],
+    }
 });
