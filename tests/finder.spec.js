a31154866ef17196e177abd20a3924f6e14bdf50
Matthew Trost <matthew@famo.us>
2015-03-30 14:15:14 -0700

    Add more finder tests

--- tests/finder.spec.js
+++ tests/finder.spec.js
@@ -3,10 +3,29 @@ var test = require('tape');
 var Finder = require('../lib/finder');
 
 test('Finder', function(t) {
-    t.plan(3);
+    t.plan(6);
     t.ok(Finder, 'exports');
     t.ok(Finder.componentURL, 'exports componentURL');
 
     var componentURL = Finder.componentURL('foo:bar:baz:qux');
     t.equals(componentURL, './components/foo/bar/baz/qux/qux.js');
+
+    var deps = Finder.findDependencies({
+        tree: '<famous:view><famous:foo:bar famous:events:click="yaya"></famous:foo:bar></famous:view>',
+        behaviors: {
+            '#foo': {
+                'famous:bla:bleep:bloop': true
+            }
+        }
+    });
+    t.deepEquals(deps, ['famous:foo:bar', 'famous:events', 'famous:foo', 'famous:bla:bleep']);
+
+    var urls = Finder.subcomponentURLs('foo:bar:baz', {
+        tree: 'sha.html',
+        behaviors: 'yay.js'
+    });
+    t.deepEquals(urls, { behaviors: './components/foo/bar/baz/yay.js', tree: './components/foo/bar/baz/sha.html' });
+
+    var type = Finder.subcomponentType('bla/blee.js');
+    t.equals(type, 'js');
 });
