6bab509cc142542863fd86b017b85e0681f659ee
Matthew Trost <matthew@famo.us>
2015-04-13 09:20:56 -0700

    Add CustomEvent polyfill for tests

---
+++ tests/helpers/custom-event-polyfill.js
@@ -0,0 +1,21 @@
+// Polyfill for creating CustomEvents on IE9/10/11
+// code pulled from:
+// https://github.com/d4tocchini/customevent-polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
+    var CustomEvent = function(event, params) {
+        var evt;
+        params = params || {
+            bubbles: false,
+            cancelable: false,
+            detail: undefined
+        };
+
+        evt = document.createEvent("CustomEvent");
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+    };
+
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent; // expose definition to window
+}
