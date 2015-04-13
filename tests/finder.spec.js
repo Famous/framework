d1f7b8413fc2992dbacd5f98f9cb8b4758a9b899
Matthew Trost <matthew@famo.us>
2015-04-13 09:21:19 -0700

    Finder now needs to be instantiated

--- tests/finder.spec.js
+++ tests/finder.spec.js
@@ -1,16 +1,17 @@
 'use strict';
 var test = require('tape');
 var Finder = require('../lib/finder');
+var finder = new Finder();
 
 test('Finder', function(t) {
     t.plan(6);
-    t.ok(Finder, 'exports');
-    t.ok(Finder.componentURL, 'exports componentURL');
+    t.ok(finder, 'exports');
+    t.ok(finder.componentURL, 'exports componentURL');
 
-    var componentURL = Finder.componentURL('foo:bar:baz:qux');
+    var componentURL = finder.componentURL('foo:bar:baz:qux');
     t.equals(componentURL, 'http://localhost:8357/foo/bar/baz/qux/qux.js');
 
-    var deps = Finder.findDependencies({
+    var deps = finder.findDependencies({
         tree: '<famous:view><famous:foo:bar famous:events:click="yaya"><la:lee:loo/></famous:foo:bar></famous:view>',
         behaviors: {
             '#foo': {
@@ -20,12 +21,12 @@ test('Finder', function(t) {
     });
     t.deepEquals(deps, ['famous:view', 'famous:foo:bar', 'famous:events', 'la:lee:loo', 'famous:bla:bleep']);
 
-    var urls = Finder.subcomponentURLs('foo:bar:baz', {
+    var urls = finder.subcomponentURLs('foo:bar:baz', {
         tree: 'sha.html',
         behaviors: 'yay.js'
     });
     t.deepEquals(urls, { behaviors: 'http://localhost:8357/foo/bar/baz/yay.js', tree: 'http://localhost:8357/foo/bar/baz/sha.html' });
 
-    var type = Finder.subcomponentType('bla/blee.js');
+    var type = finder.subcomponentType('bla/blee.js');
     t.equals(type, 'js');
 });
