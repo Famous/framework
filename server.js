d2a1035c02103aaf32445f15a3a85d91634e32fc
Matthew Trost <matthew@famo.us>
2015-04-29 11:23:40 -0700

    Enable CORS

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var express = require('express');
 
 var app = express();
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var bundle = require('./lib/bundle');
 
