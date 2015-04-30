213d3a5adb8f3839aa38e63e62a41b25fcf49c9a
Matthew Trost <matthew@famo.us>
2015-04-30 15:26:34 -0700

    Back out of the manifest store strategy

--- test/store.spec.js
+++ test/store.spec.js
@@ -3,23 +3,23 @@
 var tape = require('tape');
 var store = require('./../lib/support/store');
 
-// tape('store', function(t) {
-//     t.plan(5);
-//     t.ok(store, 'exports');
+tape('store', function(t) {
+    t.plan(5);
+    t.ok(store, 'exports');
 
-//     var filename = 'foo/bar.js';
-//     var content = 'alert(1);';
+    var filename = 'foo/bar.js';
+    var content = 'alert(1);';
 
-//     store.putFile(filename, content, function(err, result) {
-//         t.ok(result, 'file created');
-//         store.getFile(filename, function(err, result) {
-//             t.ok(result, 'file loaded');
-//             store.getData(filename, function(err, data) {
-//                 t.equals(data, content);
-//                 store.listFiles('foo', function(err, result) {
-//                     t.equals(result.Contents.length, 1);
-//                 });
-//             });
-//         });
-//     });
-// });
+    store.putFile(filename, content, function(err, result) {
+        t.ok(result, 'file created');
+        store.getFile(filename, function(err, result) {
+            t.ok(result, 'file loaded');
+            store.getData(filename, function(err, data) {
+                t.equals(data, content);
+                store.listFiles('foo', function(err, result) {
+                    t.equals(result.Contents.length, 1);
+                });
+            });
+        });
+    });
+});
