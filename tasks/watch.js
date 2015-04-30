9acfedabc98f71263bacb8b893bfdb21afccfc3e
Matthew Trost <matthew@famo.us>
2015-04-30 15:51:23 -0700

    A watch task that actually reruns

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,48 +1,15 @@
 var path = require('path');
-var sync = require('./sync');
-var validate = require('./validate');
 var watch = require('node-watch');
+var exec = require('child_process').exec;
+var sys = require('sys');
 
 var ROOT_DIR = path.join(__dirname, '..');
 var DATA_DIR = path.join(ROOT_DIR, 'data');
 var MODULES_DIR = path.join(DATA_DIR, 'modules');
 
-var SLASH = '/';
-var BLANK = '';
-var SUBDIR = '..';
-var DEFAULT_TAG = 'HEAD';
-
-// Given a directory, find the directory that is the
-// parent BEST module directory of this one.
-function getModuleDir(dir) {
-    if (validate.isModule(dir)) {
-        return dir;
-    }
-    else {
-        // No need to drop any further down than the
-        // base modules folder.
-        if (dir === MODULES_DIR) {
-            return false;
-        }
-        else {
-            return getModuleDir(path.join(dir, SUBDIR));
-        }
-    }
-}
-
 watch(MODULES_DIR, function(filename) {
-    var fullDir = path.dirname(filename);
-    var moduleDir = getModuleDir(fullDir);
-    if (moduleDir) {
-        var partialDir = moduleDir.replace(MODULES_DIR + SLASH, BLANK);
-        var baseDir = MODULES_DIR;
-        sync.single(baseDir, partialDir, DEFAULT_TAG, function(err, result) {
-            if (err) {
-                console.error(err);
-            }
-            else {
-                console.log('synced ' + partialDir, result);
-            }
-        });
-    }
+    console.log(path.basename(filename) + ' change triggers best-ecosystem resync');
+    exec('cd ' + ROOT_DIR + ' && npm run sync-all', function(err, stdout, stderr) {
+        sys.puts(stdout);
+    });
 });
