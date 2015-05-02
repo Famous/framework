95a832a277a3134adb8c1387d4c30195da67060c
Imti <imtiazmaj@gmail.com>
2015-05-01 17:15:47 -0700

    demo: wip for timeline integration

--- data/modules/imtiaz.majeed/timeline/example/example.js
+++ data/modules/imtiaz.majeed/timeline/example/example.js
@@ -8,40 +8,45 @@ BEST.module('imtiaz.majeed:timeline:example', {
             'align': [0.5, 0.5]
         },
         '#circle': {
-            'style': {
-                'background-color': 'whitesmoke',
-                'border': '1px solid grey',
-                'text-align': 'center',
-                'line-height': '10',
-                'border-radius': '50%',
-                'cursor': 'pointer'
+            'style': function(borderRadius) {
+                return {
+                    'background-color': 'whitesmoke',
+                    'border': '1px solid grey',
+                    'text-align': 'center',
+                    'line-height': '10',
+                    'border-radius': borderRadius + '%',
+                    'cursor': 'pointer'
+                }
             },
             'unselectable': true
         }
     },
     events: {
         '#circle': {
-            'famous:events:click': function($payload) {
-                console.log('click event captured');
-                //$timelines.get('timeline1').start();
+            'famous:events:click': function($timelines, $state, $payload) {
+                $timelines.get('timeline1').start();
             }
         }
     },
     states: {
-        size: [200, 200]
+        size: [200, 200],
+        borderRadius: 50,
     },
     timelines: {
         'timeline1': {
             duration: 3000,
             flexframes: {
                 '10%': {
-                    'size': [[100, 100], {curve: 'outExpo'}]
+                    'size': [[100, 100], {curve: 'outExpo'}],
+                    'borderRadius': [20, {curve: 'outBounce'}]
                 },
                 '20%': {
                     'size': [[300, 300], {curve: 'outBounce'}]
                 },
                 3000: {
-                    'size': [[200, 200], {curve: 'easeInOut'}]
+                    'size': [[200, 200], {curve: 'easeInOut'}],
+                    'borderRadius': [50, {curve: 'outExpo'}]
+
                 }
             }
         }
