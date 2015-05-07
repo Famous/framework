e0878973dd93bffad1a450a5d237071f26d5fc3e
Matthew Trost <matthew@famo.us>
2015-05-07 14:46:48 -0700

    Ensure the S3 option still works

--- config/environment.js
+++ config/environment.js
@@ -1,17 +1,18 @@
 'use strict';
 
-var ENVS = {};
-ENVS.DEVELOPMENT = 'development';
-ENVS.STAGING = 'staging';
-ENVS.PRODUCTION = 'production';
-var DEFAULT_ENV = ENVS.DEVELOPMENT;
-var kind = ENVS[process.env.ECOSYSTEM_ENV] || DEFAULT_ENV;
+var ENVS = {
+    'development': 1,
+    'production': 2
+};
+
+var DEFAULT_ENV = ENVS['development'];
+var userSpecifiedEnv = process.env.ECOSYSTEM_ENV;
+var kind = ENVS[userSpecifiedEnv] || DEFAULT_ENV;
 var PORT = 3000;
 
 module.exports = {
     kind: kind,
-    DEVELOPMENT: ENVS.DEVELOPMENT,
-    STAGING: ENVS.STAGING,
-    PRODUCTION: ENVS.PRODUCTION,
+    DEVELOPMENT: ENVS['development'],
+    PRODUCTION: ENVS['production'],
     PORT: PORT
 };
