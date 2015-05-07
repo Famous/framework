cc76c497aa3586267c57389f68a78fea0da9d495
Imti <imtiazmaj@gmail.com>
2015-05-07 16:54:22 -0700

    demos: condense example

--- data/modules/imtiaz.majeed/timeline/example/example.js
+++ data/modules/imtiaz.majeed/timeline/example/example.js
@@ -106,30 +106,19 @@ BEST.module('imtiaz.majeed:timeline:example', 'HEAD', {
             auto: true,
             duration: 1000,
             flexframes: {
-                0: {
-                    'borderRadius': [50, {curve: 'easeInOut'}]
-                },
-                '50%': {
-                    'borderRadius': [10, {curve: 'easeInOut'}]
-                },
-                1000: {
-                    'borderRadius': [50]
-                }
+                0:      { 'borderRadius': [50,  {curve: 'easeInOut'}] },
+                '50%':  { 'borderRadius': [10,  {curve: 'easeInOut'}] },
+                1000:   { 'borderRadius': [50]  }
             }
         },
         'bouncySize': {
             auto: true,
-            speed: 0.5,
+            duration: 1000,
+            speed: 0.5
             flexframes: {
-                0: {
-                    'size': [[200, 200], {curve: 'easeInOut'}],
-                },
-                '50%': {
-                    'size': [[100, 100], {curve: 'easeInOut'}]
-                },
-                1000: {
-                    'size': [[200, 200]]
-                }
+                0:      { 'size': [[200, 200],  {curve: 'outBounce'}] },
+                '50%':  { 'size': [[100, 100],  {curve: 'outBounce'}] },
+                '110%':   { 'size': [[200, 200]]  }
             }
         },
         'bouncyThickness': {
@@ -137,15 +126,9 @@ BEST.module('imtiaz.majeed:timeline:example', 'HEAD', {
             duration: 1000,
             repeat: 2,
             flexframes: {
-                0: {
-                    'borderSize': [1, {curve: 'easeInOut'}],
-                },
-                '50%': {
-                    'borderSize': [50, {curve: 'easeInOut'}]
-                },
-                1000: {
-                    'borderSize': [1]
-                }
+                0:      { 'borderSize': [1,     {curve: 'easeInOut'}] },
+                '50%':  { 'borderSize': [50,    {curve: 'easeInOut'}] },
+                1000:   { 'borderSize': [1]     }
             }
         }
     },
