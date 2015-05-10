6647c9aa1d9ec42e36589929df6499ae9c487cc9
Matthew Trost <matthew@famo.us>
2015-05-10 15:00:17 -0700

    Example of a test component with an asset

---
+++ famous/tests/assets-a/assets-a.js
@@ -0,0 +1,8 @@
+BEST.scene('famous:tests:assets-a', 'HEAD', {
+    tree: `<dom-element id="surface"></dom-element>`,
+    behaviors: {
+        '#surface': {
+            'content': '<img src="@{assets/excite.png}">'
+        }
+    }
+});
