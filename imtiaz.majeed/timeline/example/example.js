20ad2d37f17fa86de6212e49fb2f32e12aef591b
Imti <imtiazmaj@gmail.com>
2015-05-13 10:28:19 -0700

    demos: fix example to use top level timeline function

--- imtiaz.majeed/timeline/example/example.js
+++ imtiaz.majeed/timeline/example/example.js
@@ -101,36 +101,39 @@ BEST.module('imtiaz.majeed:timeline:example', 'HEAD', {
             'cursor': 'pointer'
         }
     },
-    timelines: {
-        'bouncyBorder': {
-            auto: true,
-            duration: 1000,
-            flexframes: {
-                0:      { 'borderRadius': [50,  {curve: 'easeInOut'}] },
-                '50%':  { 'borderRadius': [10,  {curve: 'easeInOut'}] },
-                1000:   { 'borderRadius': [50]  }
-            }
-        },
-        'bouncySize': {
-            auto: true,
-            duration: 1000,
-            speed: 0.5,
-            flexframes: {
-                0:      { 'size': [[200, 200],  {curve: 'outBounce'}] },
-                '50%':  { 'size': [[100, 100],  {curve: 'outBounce'}] },
-                '110%':   { 'size': [[200, 200]]  }
-            }
-        },
-        'bouncyThickness': {
-            auto: true,
-            duration: 1000,
-            repeat: 2,
-            flexframes: {
-                0:      { 'borderSize': [1,     {curve: 'easeInOut'}] },
-                '50%':  { 'borderSize': [50,    {curve: 'easeInOut'}] },
-                1000:   { 'borderSize': [1]     }
-            }
+    tree: 'example.html',
+})
+.config({
+    imports: {}
+})
+.timelines({
+    'bouncyBorder': {
+        auto: true,
+        duration: 1000,
+        flexframes: {
+            0:      { 'borderRadius': [50,  {curve: 'easeInOut'}] },
+            '50%':  { 'borderRadius': [10,  {curve: 'easeInOut'}] },
+            1000:   { 'borderRadius': [50]  }
         }
     },
-    tree: 'example.html',
+    'bouncySize': {
+        auto: true,
+        duration: 1000,
+        speed: 0.5,
+        flexframes: {
+            0:      { 'size': [[200, 200],  {curve: 'outBounce'}] },
+            '50%':  { 'size': [[100, 100],  {curve: 'outBounce'}] },
+            '110%':   { 'size': [[200, 200]]  }
+        }
+    },
+    'bouncyThickness': {
+        auto: true,
+        duration: 1000,
+        repeat: 2,
+        flexframes: {
+            0:      { 'borderSize': [1,     {curve: 'easeInOut'}] },
+            '50%':  { 'borderSize': [50,    {curve: 'easeInOut'}] },
+            1000:   { 'borderSize': [1]     }
+        }
+    }
 });
