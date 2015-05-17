11f6894a7f946b6c9451e5f72d711da15f2fc401
Imti <imtiazmaj@gmail.com>
2015-05-16 23:31:44 -0700

    demos: add basic events example to super.demo.day demos

---
+++ super.demo.day/click-event/click-event.js
@@ -0,0 +1,36 @@
+BEST.scene('super.demo.day:click-event', 'HEAD', {
+    behaviors: {
+        '#background': {
+            'align': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'style': function(background) {
+                return {
+                    'color': 'white',
+                    'background': background,
+                    'font-family': 'Lato',
+                    'font-size': '60px',
+                    'text-align': 'center',
+                    'line-height': window.innerHeight + 'px',
+                    'cursor': 'pointer',
+                    'transition': 'background 0.4s',
+                }
+            },
+            'unselectable': true
+        }
+    },
+    events: {
+        '#background': {
+            'click': function($state) {
+                var index = Math.floor(Math.random() * 8);
+                var color = $state.get(['colorPalette', index]);
+
+                $state.set('background', color);
+            }
+        }
+    },
+    states: {
+        background: '#314043',
+        colorPalette: ['#F7208B', '#ABAA98', '#82CDB9', '#F37259', '#00B9D7', '#98805B', '#728453', '#657E84']
+    },
+    tree: 'click-event.html'
+});
