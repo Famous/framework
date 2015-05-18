874e23a55aa85114b75bb29f957e784bc6f0af90
Imti <imtiazmaj@gmail.com>
2015-05-18 15:51:43 -0700

    demos: add basic lifecycle example to super.demo.day demos

---
+++ super.demo.day/lifecycle/lifecycle.js
@@ -0,0 +1,36 @@
+BEST.scene('super.demo.day:lifecycle', 'HEAD', {
+    behaviors: {
+        '#ball': {
+            'size': [400, 400],
+            'align': [0, 0.5],
+            'origin': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'position-x': function(positionX) { return positionX; },
+            'rotation-z': function(rotationZ) { return rotationZ; },
+            'style': {
+                'border-radius': '100%',
+                'border': '10px dotted #703030',
+            }
+        },
+        '#ground': {
+            'size': [window.innerWidth, 1],
+            'position-y': 200,
+            'align': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'style': { 'border': '4px solid #2F343B' }
+        }
+    },
+    events: {
+        '$lifecycle': {
+            'post-load': function($state) {
+                $state.set('positionX', window.innerWidth - 200, {duration: 10000, curve: 'outBounce'});
+                $state.set('rotationZ', Math.PI*4, {duration: 10000, curve: 'outBounce'});
+            }
+        }
+    },
+    states: {
+        'positionX': 0,
+        'rotationZ': 0
+    },
+    tree: 'lifecycle.html'
+});
