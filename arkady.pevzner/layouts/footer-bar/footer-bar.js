4de1d681420e8192dd9c646c42451e7f2f32711a
Arkady Pevzner <arkady@famo.us>
2015-05-19 18:24:05 -0700

    Update footer-bar to have 3 buttons

--- arkady.pevzner/layouts/footer-bar/footer-bar.js
+++ arkady.pevzner/layouts/footer-bar/footer-bar.js
@@ -1,32 +1,32 @@
 BEST.module('arkady.pevzner:layouts:footer-bar', 'HEAD', {
     behaviors: {
         '#background' : {
-            style: function(backgroundStyle) {
-                return backgroundStyle;
-            }
+            style: '[[identity|backgroundStyle]]'
         },
         '.button' : {
-            size: function(buttonSize) {
-                return buttonSize;
-            },
-            style: function(buttonStyle) {
-                return buttonStyle
-            },
-            'mount-point' : [0.5, 0.5],
+            'size': '[[identity|buttonSize]]',
+            'style': '[[identity|buttonStyle]]',
             'origin' : [0.5, 0.5],
             'align'  : [0.5, 0.5],
             'unselectable' : true
         },
         '#button1' : {
             'content' : '[[identity|buttonOneContent]]',
+            'mount-point' : [1, 0.5],
             'position' : function(buttonSize) {
-                return [-buttonSize[0], 0];
-            },
+                return [-buttonSize[0] , 0]
+            }
         },
         '#button2' : {
             content: '[[identity|buttonTwoContent]]',
+            'position' : [0, 0],
+            'mount-point' : [0.5, 0.5],
+        },
+        '#button3' : {
+            content: '[[identity|buttonThreeContent]]',
+            'mount-point' : [0, 0.5],
             'position' : function(buttonSize) {
-                return [buttonSize[0], 0];
+                return [buttonSize[0] , 0]
             }
         }
     },
@@ -36,7 +36,8 @@ BEST.module('arkady.pevzner:layouts:footer-bar', 'HEAD', {
             'button-style' : 'setter|camel',
             'button-size' : 'setter|camel',
             'button-one-content' : 'setter|camel',
-            'button-two-content' : 'setter|camel'
+            'button-two-content' : 'setter|camel',
+            'button-three-content' : 'setter|camel'
         },
         '#button1' : {
             'ui-click' : function($dispatcher, $payload) {
@@ -47,10 +48,15 @@ BEST.module('arkady.pevzner:layouts:footer-bar', 'HEAD', {
             'ui-click' : function($dispatcher, $payload) {
                 $dispatcher.emit('button-two-click', $payload);
             }
+        },
+        '#button3' : {
+            'ui-click' : function($dispatcher, $payload) {
+                $dispatcher.emit('button-three-click', $payload);
+            }
         }
     },
     states: {
-        style: {
+        backgroundStyle: {
             'background-color' : 'black'
         },
         buttonStyle: {
@@ -58,12 +64,8 @@ BEST.module('arkady.pevzner:layouts:footer-bar', 'HEAD', {
         },
         buttonSize: [200, 100],
         buttonOneContent: 'Button 1',
-        buttonTwoContent: 'Button 2'
+        buttonTwoContent: 'Button 2',
+        buttonThreeContent: 'Button 3',
     },
     tree: 'footer-bar.html',
-})
-.config({
-    imports: {
-        'famous:core': ['ui-element']
-    }
 });
