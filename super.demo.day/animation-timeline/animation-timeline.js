86c06cb6b639561a5e8d512490e2e5393d7ed02e
Imti <imtiazmaj@gmail.com>
2015-05-20 14:44:32 -0700

    fix: use ui-click instead of click

--- super.demo.day/animation-timeline/animation-timeline.js
+++ super.demo.day/animation-timeline/animation-timeline.js
@@ -41,7 +41,7 @@ BEST.scene('super.demo.day:animation-timeline', 'HEAD', {
      */
     events: {
         '.circle': {
-            'click': function($timelines) {
+            'ui-click': function($timelines) {
                 $timelines.get('pulse-animation').start({ duration: 1500 });
             }
         }
