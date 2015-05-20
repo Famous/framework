bf230ff657f021ca246d6a9ab10fcb8593a8443c
Imti <imtiazmaj@gmail.com>
2015-05-20 11:23:52 -0700

    docs: add docs for click-event example

--- super.demo.day/click-event/click-event.js
+++ super.demo.day/click-event/click-event.js
@@ -1,4 +1,27 @@
 BEST.scene('super.demo.day:click-event', 'HEAD', {
+    /**
+     * Behaviors:
+     *      Target the background in our tree
+     *      using selectors and set style
+     *      and other Famous properties.
+     *
+     *      Style is a function of our background state
+     *      and will rerun on each background state change.
+     *
+     *      Sugar:
+     *          'unselectable': true
+     *
+     *          ... is equivalent to ...
+     *
+     *          'style': {
+     *              '-webkit-touch-callout': 'none'
+     *              '-webkit-user-select': 'none'
+     *              '-khtml-user-select': 'none'
+     *              '-moz-user-select': 'none'
+     *              '-ms-user-select': 'none'
+     *              'user-select': 'none'
+     *          }
+     */
     behaviors: {
         '#background': {
             'align': [0.5, 0.5],
@@ -18,6 +41,21 @@ BEST.scene('super.demo.day:click-event', 'HEAD', {
             'unselectable': true
         }
     },
+    /**
+     * Events:
+     *      Target the background in our tree
+     *      using selectors and attach a click
+     *      event listener with a callback.
+     *
+     *      Inject state (denoted with a $)
+     *      into our callback function. Choose
+     *      a random index and use our injected
+     *      state to get the color value of the
+     *      colorPalette state at that index.
+     *
+     *      Then, set the background
+     *      state to that color.
+     */
     events: {
         '#background': {
             'click': function($state) {
@@ -28,6 +66,11 @@ BEST.scene('super.demo.day:click-event', 'HEAD', {
             }
         }
     },
+    /**
+     * States:
+     *      Set the background and colorPalette
+     *      states of our application.
+     */
     states: {
         background: '#314043',
         colorPalette: ['#9783F2', '#ABAA98', '#82CDB9', '#F37259', '#00B9D7', '#444444', '#728453', '#657E84']
