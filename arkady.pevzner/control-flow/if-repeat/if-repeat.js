aab3a35401bef8078c334113229b495e3e14942a
Imti <imtiazmaj@gmail.com>
2015-05-21 11:10:38 -0700

    demos: update colors on basic if-repeat example

--- arkady.pevzner/control-flow/if-repeat/if-repeat.js
+++ arkady.pevzner/control-flow/if-repeat/if-repeat.js
@@ -1,32 +1,53 @@
+var colorSteps = 36;
+var colors = [ [151, 131, 242], [47, 189, 232] ];
+
+function getColorStep(step) {
+    step -= (step >= colorSteps) ? colorSteps : 0;
+    var r = colors[0][0] - Math.round(((colors[0][0] - colors[1][0]) / colorSteps) * step);
+    var g = colors[0][1] - Math.round(((colors[0][1] - colors[1][1]) / colorSteps) * step);
+    var b = colors[0][2] - Math.round(((colors[0][2] - colors[1][2]) / colorSteps) * step);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
 BEST.module('arkady.pevzner:control-flow:if-repeat', 'HEAD', {
     behaviors: {
-        '#container' : {
-            position: '[[identity|containerPosition]]'
+        '#container': {
+            'position': [100, 100]
         },
-        '.button' : {
-            size: [75, 25],
-            style: {
-                border: '1px solid black',
-                cursor: 'pointer',
+        '.button': {
+            'size': [100, 50],
+            'style': {
+                'color': '#F2F2F0',
+                'border': '1px solid #7099EE',
+                'cursor': 'pointer',
                 'text-align' : 'center',
-                'font-family' : 'monospace',
-                'line-height' : '25px'
+                'font-family' : 'Lato',
+                'line-height' : '50px',
+                'font-size': '20px',
+                'border-radius': '5px'
             },
             'unselectable' : true
         },
-        '#add-button' : {
-            position: [80, 0]
+        '#add-button': {
+            'position': [110, 0]
         },
-        '#remove-button' : {
-            position: [160, 0]
+        '#remove-button': {
+            'position': [220, 0]
         },
-        '#blocks-view' : {
-            position: [0, 50],
-            '$if' : function (toggleState) {
-                return toggleState;
+        '#toggle-button': {
+            'position': [0, 0],
+            'template': function(showState) {
+                var text = showState ? 'Hide' : 'Show';
+                return { toggleText: text };
             }
         },
-        '.block' : {
+        '#blocks-view': {
+            'position': [25, 100],
+            '$if' : function (showState) {
+                return showState;
+            }
+        },
+        '.block': {
             size: function (blockSize) {
                 return blockSize;
             },
@@ -40,34 +61,37 @@ BEST.module('arkady.pevzner:control-flow:if-repeat', 'HEAD', {
                 }
                 return result;
             },
-            style: {
-                'text-align' : 'center',
-                'line-height' : '100px',
-                'font-size' : '24px',
-                'font-weight' : 'bold',
-                'border': '5px solid black',
-                'box-sizing': 'border-box',
-                'background-color' : 'gray',
-                'color' : 'white'
+            style: function($index) {
+                return {
+                    'text-align' : 'center',
+                    'line-height' : '100px',
+                    'font-size' : '24px',
+                    'font-weight' : 'bold',
+                    'border': '5px solid #333333',
+                    'box-sizing': 'border-box',
+                    'font-family': 'Lato',
+                    'background-color' : getColorStep($index),
+                    'color' : '#F2F2F0'
+                }
             }
         }
     },
     events: {
-        '#toggle-button' : {
+        '#toggle-button': {
             'ui-click': function($state) {
-                $state.set('toggleState', !$state.get('toggleState'));
+                $state.set('showState', !$state.get('showState'));
             }
         },
-        '#add-button' : {
+        '#add-button': {
             'ui-click': function($state) {
-                if ($state.get('toggleState')) {
+                if ($state.get('showState')) {
                     $state.chain('blockCount').add(1);
                 }
             }
         },
-        '#remove-button' : {
+        '#remove-button': {
             'ui-click': function($state) {
-                if ($state.get('toggleState') && $state.get('blockCount') > 0) {
+                if ($state.get('showState') && $state.get('blockCount') > 0) {
                     $state.chain('blockCount').subtract(1);
                 }
             }
@@ -77,7 +101,7 @@ BEST.module('arkady.pevzner:control-flow:if-repeat', 'HEAD', {
         containerPosition: [100, 100],
         blockSize: [260, 100],
         blockCount: 5,
-        toggleState: true
+        showState: true
     },
     tree: 'if-repeat.jade',
 });
