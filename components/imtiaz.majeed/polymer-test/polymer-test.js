ea11a0ef15417cd495f15547fce2f1134e2f0359
Imti <imtiazmaj@gmail.com>
2015-05-27 13:48:27 -0700

    experiment: add polymer-test

---
+++ components/imtiaz.majeed/polymer-test/polymer-test.js
@@ -0,0 +1,15 @@
+BEST.scene('imtiaz.majeed:polymer-test', 'HEAD', {
+    behaviors: {
+        '#spinner': {
+            'size': [100, 100],
+            'style': {
+                'background': 'red'
+            },
+            'content': 'polymer paper component here'
+        }
+    },
+    events: {},
+    states: {},
+    tree: 'polymer-test.html'
+})
+// .config({ includes: ['components/paper-spinner/paper-spinner.html'] })
