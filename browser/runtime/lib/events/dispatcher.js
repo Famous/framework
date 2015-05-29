6cb93466b38c47443f869f7460da4e3ea4a6ff34
Imti <imtiazmaj@gmail.com>
2015-05-29 15:40:40 -0700

    feat: add ability to broadcast events using dispatcher

--- browser/runtime/lib/events/dispatcher.js
+++ browser/runtime/lib/events/dispatcher.js
@@ -37,6 +37,45 @@ Dispatcher.prototype.emit = function(key, message) {
     }
 };
 
+Dispatcher.willEventsTrickle = (function() {
+    var willTrickle = false;
+    var doc = window.document;
+    if (doc) {
+        var parent = doc.createElement('div');
+        var child = parent.cloneNode();
+        parent.appendChild(child);
+        child.addEventListener('e', function() {
+            willTrickle = true;
+        });
+
+        parent.dispatchEvent(new CustomEvent('e', { trickles: true }));
+    }
+    return willTrickle;
+})()
+
+
+Dispatcher.prototype.broadcast = function(key, message) {
+    var element =  this.domNode;
+    var event = new CustomEvent(key, {
+        detail: message,
+        trickles: true
+    });
+    if (Dispatcher.willEventsTrickle) {
+        element.dispatchEvent(event)
+    }
+    else {
+        var broadcastToChildNodes = function(node) {
+            for (var i = 0; i < node.childNodes.length; i++) {
+                var child = node.childNodes[i];
+                child.dispatchEvent(event);
+                broadcastToChildNodes(child);
+            }
+        }
+
+        broadcastToChildNodes(element);
+    }
+};
+
 Dispatcher.prototype.on = function(key, cb) {
     this.domNode.addEventListener(key, cb);
 };
