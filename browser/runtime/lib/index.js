36fceed00831627c0e2489dae3ee20ee8c2d7950
Matthew Trost <matthew@famo.us>
2015-05-31 11:36:06 -0700

    Allow for more flexible arguments to BEST.register

--- browser/runtime/lib/index.js
+++ browser/runtime/lib/index.js
@@ -7,7 +7,40 @@ var helpers = require('./helpers/helpers');
 
 var deployer = new Deployer();
 
+// We use the module's tag for reference, so we need to give
+// any modules that happen to not have a tag a default one.
+var UNSPECIFIED_TAG = '!unspecified!';
+var OBJECT_TYPE = 'object';
+
+function returnUnspecifiedTag(name, tag) {
+    console.warn('Tag given for `' + name + '` was `' + tag + '`; defaulting to `unspecified` instead');
+    return UNSPECIFIED_TAG;
+}
+
 function register(name, tag, definition) {
+    // Allow the arguments to `register` to be flexible, i.e. allow:
+    // BEST.scene('foo:bar') (e.g. for a timeline-only API)
+    // BEST.scene('foo:bar', {...}),
+    // BEST.scene('foo:bar', '0.1', {...})
+    if (!definition) {
+        if (!tag) {
+            definition = {};
+            tag = returnUnspecifiedTag(name tag);
+        }
+        else {
+            if (typeof tag === OBJECT_TYPE) {
+                definition = tag;
+                tag = returnUnspecifiedTag(name, tag);
+            }
+            else {
+                definition = {};
+                if (typeof tag !== 'string') {
+                    tag = returnUnspecifiedTag(name, tag);
+                }
+            }
+        }
+
+    }
     return DataStore.registerModule(name, tag, definition);
 }
 
