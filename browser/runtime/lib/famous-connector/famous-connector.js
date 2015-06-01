127da8890b523ae2aca8345b624c4dd283c33dd0
Larry Gordon <lgordon@psyrendust.com>
2015-06-01 11:01:13 -0700

    Fixes lint error "Famous is defined but never used" in famous-connector.js

--- browser/runtime/lib/famous-connector/famous-connector.js
+++ browser/runtime/lib/famous-connector/famous-connector.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Famous = window.Famous = require('famous');
+window.Famous = require('famous');
 var FamousEngine = window.FamousEngine = require('famous/core/FamousEngine');
 
 FamousEngine.init();
