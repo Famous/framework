d9f1a47ff9917e8bf0697bddf0df0c0c6ca637bd
Larry Gordon <lgordon@psyrendust.com>
2015-06-01 11:08:03 -0700

    Changes indentation for query-string.js from 2 space to 4 space to match .editorconfig

--- local/workspace/build/assets/query-string.js
+++ local/workspace/build/assets/query-string.js
@@ -1,33 +1,33 @@
 'use strict';
 
 (function() {
-  var SEARCH = window.location.search;
-  var QUERY = (function(a) {
-    if (a === '') {
-      return {};
-    }
-    if (a[0] === '') {
-      a.shift();
-    }
-    var b = {};
-    for (var i = 0; i < a.length; ++i) {
-      var p = a[i].split('=', 2);
-      if (p.length === 1) {
-        b[p[0]] = '';
-      }
-      else {
-        var s = p[1].replace(/\+/g, ' ');
-        var sd = decodeURIComponent(s);
-        if (sd === 'false') {
-          sd = false;
+    var SEARCH = window.location.search;
+    var QUERY = (function(a) {
+        if (a === '') {
+            return {};
         }
-        else if (sd === 'true') {
-          sd = true;
+        if (a[0] === '') {
+            a.shift();
         }
-        b[p[0]] = sd;
-      }
-    }
-    return b;
-  })(SEARCH.substr(1).split('&'));
-  window.QUERY = QUERY;
+        var b = {};
+        for (var i = 0; i < a.length; ++i) {
+            var p = a[i].split('=', 2);
+            if (p.length === 1) {
+                b[p[0]] = '';
+            }
+            else {
+                var s = p[1].replace(/\+/g, ' ');
+                var sd = decodeURIComponent(s);
+                if (sd === 'false') {
+                    sd = false;
+                }
+                else if (sd === 'true') {
+                    sd = true;
+                }
+                b[p[0]] = sd;
+            }
+        }
+        return b;
+    })(SEARCH.substr(1).split('&'));
+    window.QUERY = QUERY;
 }());
