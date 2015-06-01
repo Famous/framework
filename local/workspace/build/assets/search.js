a1da80932e9815397ee7699eb2cae1b143141bf8
Larry Gordon <lgordon@psyrendust.com>
2015-06-01 09:36:59 -0700

    Refactors menu for `workspace/build/index.html` * Adds Eric Meyer's Reset CSS v2.0 `workspace/build/assets/reset.css` and `workspace/build/assets/reset.min.css` to repo * Adds `workspace/build/assets/search-icon.svg` to repo * Adds `workspace/build/assets/search.css` to repo * Adds `workspace/build/assets/search.js` to repo * Adds `[unresolved]` class to `index.html` to prevent FOUC * Updates `workspace/build/index.html` to use `search.js`, `search.css`, `search-icon.svg` and `reset.min.css` * Changes `datalist` to `ul` for wider browser support

---
+++ local/workspace/build/assets/search.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function() {
+  var searchContainer = document.getElementById('best-components-search');
+  var searchBtn = searchContainer.querySelector('.best-components-search-btn');
+  var searchDropdown = searchContainer.querySelector('.best-components-search-dropdown');
+  var searchInput = searchContainer.querySelector('input');
+  var searchScenes = searchContainer.querySelector('ul');
+  var searchScenesOptions = searchScenes.querySelectorAll('li');
+  var stage = document.getElementById('best-stage');
+  searchBtn.addEventListener('click', function() {
+    if (searchContainer.className === '') {
+      searchContainer.className = 'active';
+    }
+    else {
+      searchContainer.className = '';
+    }
+  });
+  searchDropdown.addEventListener('click', function() {
+    if (searchContainer.className === 'active') searchContainer.className = 'active expanded';
+    else searchContainer.className = 'active';
+  });
+  [].forEach.call(searchScenesOptions, function(li) {
+    li.addEventListener('click', function(event) {
+      searchInput.value = event.currentTarget.textContent;
+      window.location.search = '?best=' + event.currentTarget.textContent;
+    });
+  });
+  searchInput.value = QUERY.best || 'hello-famous';
+
+  if (QUERY.best) {
+    if (QUERY.best.split(':').length < 2) QUERY.best = 'super.demo.day:' + QUERY.best;
+    BEST.deploy(QUERY.best, 'HEAD', '#best-stage');
+  }
+  else {
+    BEST.deploy('super.demo.day:hello-famous', 'HEAD', '#best-stage');
+  }
+
+  // Hack to mask FOUC when using align
+  setTimeout(function() { stage.removeAttribute('unresolved'); }, 1000);
+}());
