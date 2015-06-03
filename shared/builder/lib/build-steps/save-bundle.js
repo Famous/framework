b858a029be9af81157cd0003769920687a323b57
Matthew Trost <matthew@famo.us>
2015-06-03 10:32:49 -0700

    Correct typo of reference to version ref

--- shared/builder/lib/build-steps/save-bundle.js
+++ shared/builder/lib/build-steps/save-bundle.js
@@ -7,7 +7,7 @@ function saveBundle(where, info, cb) {
         info.bundleVersionRef = localSaveInfo.bundleVersionRef;
         info.bundlePath = localSaveInfo.bundlePath;
         info.bundleURL = localSaveInfo.bundleURL;
-        console.log('Built bundle of `' + info.name + '` (' + info.bundleRef + ') to ' + info.bundlePath);
+        console.log('Built bundle of `' + info.name + '` (' + info.bundleVersionRef + ') to ' + info.bundlePath);
         cb(null, info);
     });
 }
