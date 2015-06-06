35a56dede0bb492751f268429956a8c4354f5364
Matthew Trost <matthew@famo.us>
2015-06-06 09:40:54 -0700

    Show the image in the large asset test

--- components/famous/tests/assets-large/assets-large.js
+++ components/famous/tests/assets-large/assets-large.js
@@ -1,2 +1,6 @@
-BEST.module('famous:tests:assets-large', {});
-
+BEST.module('famous:tests:assets-large', {
+    tree: `
+        <h1>You should see a large image of a galaxy here</h1>
+        <img src="@{galaxy.jpg}">
+    `
+});
