8b3d413a91794ece43c440c945329b8c0c50c96e
Matthew Trost <matthew@famo.us>
2015-06-08 14:42:28 -0700

    Add test showing multiple CSS files loaded

---
+++ components/famous-tests/include-css/include-css.js
@@ -0,0 +1,18 @@
+BEST.scene('famous-tests:include-css', {
+    tree: `
+        <h1 id="my-h1">Testing that CSS is included via includes correctly</h1>
+        <h2>You should see a blug background, white large header text, and three orange underlined ahoys.</h2>
+        <p class="p">Ahoy</p>
+        <p class="p">Ahoy</p>
+        <p class="p">Ahoy</p>
+    `
+})
+.config({
+    includes: [
+        'a.css',
+        'b.css',
+        'c.css',
+        'd.css',
+        'e.css'
+    ]
+});
