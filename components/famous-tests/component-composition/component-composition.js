7591952d3d111b3a7ea049a255dc6cc5a01a7f1e
Larry Gordon <lgordon@psyrendust.com>
2015-06-09 14:51:05 -0700

    Adds component-composition test

---
+++ components/famous-tests/component-composition/component-composition.js
@@ -0,0 +1,28 @@
+BEST.scene('famous-tests:component-composition', {
+    behaviors: {
+        '#box1': {
+            'size': [200, 200],
+            'position': [0, 0],
+            'style': {
+                background: 'rgba(255, 0, 0, 0.9)'
+            }
+        },
+        '#box2': {
+            'size': [200, 200],
+            'position': [50, 50],
+            // 'style': {
+            //     background: 'rgba(0, 255, 0, 0.9)'
+            // }
+        },
+        '#child1': {
+            'size': [200, 200],
+            'position': [250, 250],
+            'style': {
+                background: 'rgba(0, 0, 255, 0.9)'
+            }
+        }
+    },
+    events: {},
+    states: {},
+    tree: 'component-composition.html'
+});
