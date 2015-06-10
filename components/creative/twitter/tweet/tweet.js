1f6f536399784955832a69e4f8a21885ee024b25
Jordan Papaleo <papaleowebdev@gmail.com>
2015-06-10 14:04:21 -0700

    Tweets on page again

--- components/creative/twitter/tweet/tweet.js
+++ components/creative/twitter/tweet/tweet.js
@@ -1,40 +1,39 @@
 BEST.scene('creative:twitter:tweet', {
     behaviors: {
         '.tweet': {
-            'size-absolute-y': (sizeY) => {
-                //'[[identity|sizeY]]',
-                return sizeY;
-            },
-
+            'size-absolute-y': '[[identity|sizeY]]',
+            'position-z': '[[identity|positionZ]]',
             'position-y': '[[identity|positionY]]',
-            styles: {
-                'padding': '10px 12px 5px 70px',
-                'border-bottom': '1px solid rgb(229, 235, 239)'
+            style: (positionZ) => {
+                console.log('positionZ',positionZ);
+                return {
+                    'padding': '10px 12px 5px 70px',
+                    'border-bottom': '1px solid rgb(229, 235, 239)',
+                    'z-index': positionZ
+                }
             },
-             'template': (model) => {
-                 return {
-                     userImage: '{{@CDN_PATH}}' + model.imageURL,
-                     displayName: model.displayName,
-                     userName: model.userName,
-                     tweetContent: model.tweetContent,
-                     tweetImage: '{{@CDN_PATH}}' + model.tweetImage,
-                     tweetAge: model.tweetAge,
-                     retweets: model.retweets,
-                     favorites: model.favorites
-                 };
-             }
+            'template': (model) => {
+                 //console.log('model',model);
+                 /*{
+                      imageURL:
+                      displayName:
+                      userName:
+                      tweetContent:
+                      tweetImage:
+                      tweetAge:
+                      retweets:
+                      favorites:
+                  }*/
+                return model;
+            }
         }
     },
     events: {
         '$public': {
-            'sizeY': '[[setter]]',
+            'sizeY':     '[[setter]]',
             'positionY': '[[setter]]',
-            'model': '[[setter]]'
-        },
-        '$pass-through' : {
-            '.tweet-view' : {
-                'position-my-tweet' : 'position'
-            }
+            'positionZ': '[[setter]]',
+            'model':     '[[setter]]'
         }
     },
     states: {},
