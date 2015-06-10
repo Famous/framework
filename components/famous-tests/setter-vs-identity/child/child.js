d27b5a6d36f1f4861730a125ad4a4d15b210a50d
Imti <imtiazmaj@gmail.com>
2015-06-10 15:21:10 -0700

    example: add example of shorthand warning to famous-tests

---
+++ components/famous-tests/setter-vs-identity/child/child.js
@@ -0,0 +1,19 @@
+BEST.scene('famous-tests:setter-vs-identity:child', {
+    behaviors: {
+        '#square': {
+            'size': '[[identity|squareSize]]', // try changing this to '[[setter|squareSize]]'
+            'style': { 'background': 'whitesmoke' }
+        }
+    },
+    events: {
+        '$public': {
+            'childSize': '[[setter|squareSize]]' // try changing this to '[[identity|squareSize]]'
+        }
+    },
+    states: {
+        'squareSize': [200, 200]
+    },
+    tree: `
+        <node id="square"></node>
+    `
+});
---
+++ components/famous-tests/setter-vs-identity/parent/parent.js
@@ -0,0 +1,17 @@
+BEST.scene('famous-tests:setter-vs-identity:parent', {
+    behaviors: {
+        '#child': {
+            'childSize': [300, 300]
+        }
+    },
+    events: {},
+    states: {},
+    tree: `
+        <child id="child"></child>
+    `
+})
+.config({
+    imports: {
+        'famous-tests:setter-vs-identity': ['child']
+    }
+});
