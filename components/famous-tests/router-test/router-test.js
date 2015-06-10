775c895341b4175aabf38ce7a0805c38a641249f
Imti <imtiazmaj@gmail.com>
2015-06-10 13:20:00 -0700

    example: Add route example to famous-tests

---
+++ components/famous-tests/router-test/router-test.js
@@ -0,0 +1,27 @@
+BEST.scene('famous-tests:router-test', {
+    behaviors: {
+        '#home': {
+            '$if': function($route) { return $route === '/home'  }
+        },
+        '#blog': {
+            '$if': function($route) { return $route === '/blog'  }
+        },
+        '#about': {
+            '$if': function($route) { return $route === '/about' }
+        },
+        '#fourOhFour': {
+            '$if': function($route) { return $route != '/home' && $route != '/blog' && $route != '/about' }
+        },
+        '.page': {
+            'size': [200, 200],
+            'align': [0.5, 0.5],
+            'mount-point': [0.5, 0.5],
+            'style': {
+                'background': 'whitesmoke'
+            }
+        }
+    },
+    events: {},
+    states: {},
+    tree: 'router-test.html'
+});
